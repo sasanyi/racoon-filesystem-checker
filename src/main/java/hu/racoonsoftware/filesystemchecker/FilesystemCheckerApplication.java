7aab7c67390dabdc2e8a9379cc47585dc5a8626d
sasanyi <savanya.sandor.jozsef@gmail.com>
2024-02-24 18:14:40 +0100

    Init SpringBoot project

---
+++ src/main/java/hu/racoonsoftware/filesystemchecker/FilesystemCheckerApplication.java
@@ -0,0 +1,13 @@
+package hu.racoonsoftware.filesystemchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilesystemCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilesystemCheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/racoonsoftware/filesystemchecker/FilesystemCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.racoonsoftware.filesystemchecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilesystemCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
